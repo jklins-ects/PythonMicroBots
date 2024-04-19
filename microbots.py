9c169e233d018dcf3159f3f12b3b5e4e5922c6cf
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2024-04-18 21:29:54 -0400

    minor cleanup

--- microbots.py
+++ microbots.py
@@ -2,34 +2,36 @@ from __future__ import annotations
 from colorama import Fore, Style
 
 class Cell:
-    def __init__(self, color:str, val:int):
-        self.color = color
-        self.val = val
-        self.row:int
-        self.col:int
+    """used to encapsulate properties of each individual space on a microbots board"""
+    def __init__(self, color:str, val:int, row=-1, col = -1):
+        self.color:str = color
+        self.val:int = val
+        self.row:int = row
+        self.col:int = col
 
-    def similar(self, other:Cell):
+    def similar(self, other:Cell) -> bool:
         """returns true if the val or color are the same"""
         return self.color == other.color or self.val == other.val
     
-    def __str__(self):
+    def __str__(self) -> str:
+        """returns a colored string with the value of the cell"""
         return f'{self.color}{self.val}{Style.RESET_ALL}'
 
-def print_cell(cell:Cell):
+def print_cell(cell:Cell) -> None:
     print(cell, end=' ')
 
-def build_board(board:list):
+def print_board(board:list) -> None:
     """prints the board to screen. Also, assigns row/col to each cell (this was a patch)"""
     for row in range(len(board)):
         for col in range(len(board[row])):
             print_cell(board[row][col])
-            #doing this to make life easier
+            #doing this to make life easier...should be set at instantiation
             board[row][col].row = row
             board[row][col].col = col
         print()
 
 
-def find_possible_moves(t1:Cell, board:list):
+def find_possible_moves(t1:Cell, board:list) -> list:
     """checks for similar cells in the provided cells row and column"""
     possible = []
     for i in range(len(board)):
@@ -42,7 +44,7 @@ def find_possible_moves(t1:Cell, board:list):
             possible.append(board[t1.row][j])
     return possible
 
-def find_shortest_path(start:Cell, end:Cell, board:list, visited:list = []):
+def find_shortest_path(start:Cell, end:Cell, board:list, visited:list = []) -> list:
     """recursively searches for the shortest path to the end with the given start"""
     if start == end:
         visited = visited + [start]
@@ -63,8 +65,8 @@ def find_shortest_path(start:Cell, end:Cell, board:list, visited:list = []):
        
         return shortest
 
-
-def main():
+def get_board() -> list:
+    """returns a 2d list of cells in the microbots order"""
     board = [
         [Cell(Fore.MAGENTA, 5), Cell(Fore.MAGENTA, 2), Cell(Fore.MAGENTA, 3), Cell(Fore.YELLOW, 2), Cell(Fore.WHITE, 1), Cell(Fore.BLUE, 1)],
         [Cell(Fore.RED, 6), Cell(Fore.RED, 4), Cell(Fore.YELLOW, 3),Cell(Fore.WHITE, 2), Cell(Fore.GREEN, 2), Cell(Fore.RED, 2)],
@@ -73,10 +75,18 @@ def main():
         [Cell(Fore.RED, 3), Cell(Fore.YELLOW, 4), Cell(Fore.WHITE, 3), Cell(Fore.RED, 1), Cell(Fore.BLUE, 5), Cell(Fore.YELLOW, 6)],
         [Cell(Fore.BLUE, 3), Cell(Fore.GREEN, 4), Cell(Fore.WHITE, 5), Cell(Fore.GREEN, 1), Cell(Fore.RED, 5), Cell(Fore.YELLOW, 1)],
     ]
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            board[row][col].row = row
+            board[row][col].col = col
+    return board
+
+def main() -> None:
+    board = get_board()
     again = ""
     while again.lower() != "q":
         try:
-            build_board(board)
+            print_board(board)
             print("For entering the following coordinates, the rows/cols are 0 indexed")
             start = input("Enter start row,col -> ").split(",")
             end = input("Enter end row,col -> ").split(",")
@@ -89,7 +99,7 @@ def main():
                 print(str(c), end=" ")
             print()
         except:
-            print("An error occurred. Make sure your input is in the form #,#")
+            print("An error occurred. Make sure your input is in range and in the form #,#")
         again = input("Enter q to quit or anything else to go again -> ")
     
 
